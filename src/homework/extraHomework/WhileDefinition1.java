5271da5fea8ccd0e046541edb06ca7333fcff592
SaizeAnastasia <saize_anastasia@mail.de>
2024-01-22 14:56:01 +0100

    lesson1 update

---
+++ src/homework/extraHomework/WhileDefinition1.java
@@ -0,0 +1,14 @@
+package homework.extraHomework;
+
+public class WhileDefinition1 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+    System.out.println(i);
+                i++;
+            }
+        }
+
+    }
+
+
